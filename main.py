aa4a203bb4bdf8f634a47a3f5dc449e85c9dbdac
nerdz0 <nerdspective0@gmail.com>
2026-03-14 14:08:34 +0000

    addition of  modules folder

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-# main.py
+import pymysql 
+
+import modules.login as login
+
+user_name = input(":: Enter user name >>> ")
+user_password = input(f":: Enter password for {user_name}>>> ")
+
+try:
+    connection_object = pymysql.Connection(
+        host = "localhost",
+        user = user_name,
+        password = user_password,
+        database = employees
+    )
+
+    if connection_object.open():
+        print("--- Successfully Connected to Database ---")
+
+except Exception as error:
+    print(f"Error :: {error}")
+
+while True:
+    
+    login.login_as()
---
+++ modules/login.py
@@ -0,0 +1,21 @@
+def login_as():
+    print("==================================")
+    print("    Employee Management System    ")
+    print("==================================")
+    print("[1]    Login as Administrator")
+    print("[2]    Login as Manager")
+    print("[3]    Login as Emplyee")
+    print("[4]    Exit")
+    choice = input("\n:: Select option >>> ")
+    
+    if choice == '1':
+        print("\n--- Logged in as Administrator ---\n")
+    
+    elif choice == '2':
+        print("\n--- Logged is as Manager ---\n")
+
+    elif choice == '3':
+        print("\n--- Logged in as Employee ---\n")
+
+    elif choice == '4':
+        print("\n--- Thank you ---\n")
